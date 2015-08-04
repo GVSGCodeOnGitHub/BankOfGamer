297a1e9804b238996254093c102e5e6b8f6e7d00
Matthew Ronchetto <matthew.ronchetto678@gmail.com>
2015-08-03 17:18:21 -0700

    Update bank_boot.bat

--- PC/bank_boot.bat
+++ PC/bank_boot.bat
@@ -1,5 +1,9 @@
 @ECHO OFF
+color 0a
+title Bank Downloader
 echo Welcome to Bank Setup!
 echo Please click any key to download files!!!
 PAUSE
-wget -outf bank_run.bat http://download.mediafire.com/
+wget -outf bank_setup.bat http://download1335.mediafire.com/nwl6vavu1bwg/dc1itam65dkbp6a/bank_setup.bat
+echo Download Complete!!! Please go to the batch on your desktop!!!
+PAUSE
